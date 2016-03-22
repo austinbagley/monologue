969bd5f079c0e2e29e32e200c021e99996e79afc
austinbagley <austin.bagley@gmail.com>
2016-03-22 17:15:45 -0600

    Delete 20160322190957_add_url_and_title_to_monologue_user.rb

--- db/migrate/20160322190957_add_url_and_title_to_monologue_user.rb
+++
@@ -1,6 +0,0 @@
-class AddUrlAndTitleToMonologueUser < ActiveRecord::Migration
-  def change
-    add_column :monologue_users, :position, :string
-    add_column :monologue_users, :img_url, :string
-  end
-end
